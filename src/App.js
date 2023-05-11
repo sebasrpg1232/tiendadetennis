0d4b6b1dc1f0fcf9b0c3e54808ffeab61e565492
sebasrpg1232 <sebasrpg1232@gmail.com>
2023-05-10 22:00:41 -0500

    installing and adding router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Menu from './Menu/Menu';
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "./Components/Menu/Menu";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Menu></Menu>
+      <nav>
+        <Router>
+          <Menu>
+            <Routes>
+              <Route exact path="/"></Route>
+            </Routes>
+          </Menu>
+        </Router>
+      </nav>
     </div>
   );
 }
