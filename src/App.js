d81c89401aa4762c0cb57cc6d989aea6b5bc28d1
ChMartin24 <martinchaverra24@gmail.com>
2023-05-11 16:59:21 -0500

    Componente VerTenis (No funcional)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./Components/Menu/Menu";
-import Home from "./Components/Home/Home"
+import Home from "./Components/Home/Home";
 import CrearProducto from "./Components/CrearProducto/CrearProducto";
+import VerTenis from "./Components/VerTenis/VerTenis";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home/>}></Route> 
               <Route path="/CrearProducto" element={<CrearProducto/>}></Route>
-              <Route path="/VerTenis"></Route>
-              <Route path="/ActualizarStock"></Route>
+              <Route path="/VerTenis" element={<VerTenis/>}></Route>
+              <Route path="/ActualizarStock" element={<CrearProducto/>}></Route>
             </Routes>
         </Router>
       </nav>
---
+++ src/Components/VerTenis/VerTenis.js
@@ -0,0 +1,28 @@
+function VerTenis (){
+    return(
+    <div>
+        <table class="table" id="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre Referencia</th>
+                <th scope="col">Color</th>
+                <th scope="col">Talla</th>
+                <th scope="col">Precio</th>
+              </tr>
+            </thead>
+            <tbody>
+             <tr>
+                <th scope="row">1</th>
+                 <td>Los naik del seba</td>
+                 <td>verde biche fosforescente</td>
+                 <td>90</td>
+                 <td>10 peso</td>
+             </tr>
+            </tbody>
+          </table>
+    </div>
+    )
+}
+
+export default VerTenis
