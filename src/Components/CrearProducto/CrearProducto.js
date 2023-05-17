b14b7ca1c9a0ec14d34818438d080eb1d9337a8b
LAPTOP <sebasrpg1232@gmail.com>
2023-05-17 10:48:08 -0500

    mejora visual

--- src/Components/CrearProducto/CrearProducto.js
+++ src/Components/CrearProducto/CrearProducto.js
@@ -29,22 +29,29 @@ const handleClick = (evt) => {
         setValidationMessages([]);
         let messages = [];
         if (!referencia) {
-            messages.push("referencia is required");
+            messages.push("Referencia is required");
         }
         if (!color) {
-            messages.push("color is required");
+            messages.push("Color is required");
         }
         if(!talla){
-          messages.push("talla is required")
+          messages.push("Talla is required")
         }
         if(!precio){
-          messages.push("precio is required")
+          messages.push("Precio is required")
         }
         setValidationMessages(messages);
       }
     return (
-      <div class="container-sm">
-      <form >
+      <div class="vh-100" style={{backgroundColor: "#eee"}}>
+      <form class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style={{borderRadius: "25px"}}>
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+              <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Crear Producto</p>
         <div class="input-group flex-nowrap">
           <span class="input-group-text" style={spanStyle} id="addon-wrapping">
             Nombre de la Referencia
@@ -59,6 +66,9 @@ const handleClick = (evt) => {
             name="referencia"
           ></input>
         </div>
+        <div>
+          {validationMessages.length > 0 && <span class="badge bg-danger"> {validationMessages[0]}</span>}
+          </div>
         <div class="input-group flex-nowrap">
           <span class="input-group-text" style={spanStyle} id="addon-wrapping">
             Color del Tenis
@@ -73,6 +83,9 @@ const handleClick = (evt) => {
             name="color"
           ></input>
         </div>
+        <div>
+          {validationMessages.length > 0 && <span class="badge bg-danger"> {validationMessages[1]}</span>}
+          </div>
         <div class="input-group flex-nowrap">
           <span class="input-group-text" style={spanStyle} id="addon-wrapping">
             Talla del tenis
@@ -87,6 +100,9 @@ const handleClick = (evt) => {
             name="talla"
           ></input>
         </div>
+        <div>
+          {validationMessages.length > 0 && <span class="badge bg-danger"> {validationMessages[2]}</span>}
+          </div>
         <div class="input-group flex-nowrap">
           <span class="input-group-text" style={spanStyle} id="addon-wrapping">
             Precio del tenis
@@ -101,15 +117,28 @@ const handleClick = (evt) => {
             name="precio"
           ></input>
         </div>
-        <button type="button" onClick={handleClick}>Save</button>
+        <div>
+          {validationMessages.length > 0 && <span class="badge bg-danger"> {validationMessages[3]}</span>}
+          </div>
+        <button type="button" onClick={handleClick} class="btn btn-dark">Guardar</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
       </form>
-      <div>{validationMessages.length > 0 && <span>Validation Summary</span>}
-                <ul>
-                  <span class="text-danger">
-                  {validationMessages.map(vm => <li key={vm}>{vm}</li>)}
-                  </span>
-                </ul>
-            </div>
+      {/* <div>{validationMessages.length > 0 && <span>Validation Summary</span>}
+                
+                  
+                  
+                  <h6>Errores de validacion: {validationMessages.map(vm => (<div>
+                    <span class="badge bg-danger" key={vm}> {vm} </span>
+                    </div>))}</h6> */}
+                  
+                  
+                
+            {/* </div> */}
             </div>
     );
   
