b23cc99e201bae50d0a271604f66d637b11d0f63
ChMartin24 <martinchaverra24@gmail.com>
2023-05-17 21:38:05 -0500

    Cards terminas en Home

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,13 +1,34 @@
 
 function Cards(props){
     return(
-    <div class="card" style={{width:"18rem"}}>
-        <img class="card-img-top" src ={require(`../Images/card_${props.image}`)} alt={props.altImg}/>
-        <div class="card-body">
-    <h5 class="card-title">{props.titulo}</h5>
-    <p class="card-text">{props.texto}</p>
-    <a href="#" class="btn btn-primary">Ver tennis</a>
+  //   <div class="card bg-dark text-light" style={{width:"18rem"}}>
+  //       <img class="card-img-top" src ={require(`../../Imagenes/tarjeta${props.img}.jpg`)} alt={props.altImg}/> 
+  //       <div class="card-body">
+  //   <h5 class="card-title">{props.titulo}</h5>
+  //   <p class="card-text">{props.precio}</p>
+  //   <a href="#" class="btn btn-light">Ver Referencia</a>
+  // </div>
+  //   </div>
+
+  <div class="text-light card mb-3 ms-3 bg-dark" style={{maxWidth: '540px'}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="img-fluid rounded-start"src ={require(`../../Imagenes/tarjeta${props.img}.jpg`)} alt={props.altImg}/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{props.titulo}</h5>
+        <p class="card-text">{props.precio}</p>
+        <a href="#" class="btn btn-light">Ver Referencia</a>
+      </div>
+    </div>
   </div>
-    </div>);
+</div>
+  
+  
+  
+  
+  
+  );
 }
 export default Cards
