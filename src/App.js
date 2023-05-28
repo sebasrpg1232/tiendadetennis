0a1af0058fc22efe12b839b0ee6ce0617eb18a30
ChMartin24 <martinchaverra24@gmail.com>
2023-05-28 16:52:09 -0500

    Módulo Login terminado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Menu from "./Components/Menu/Menu";
 import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
 import CrearProducto from "./Components/CrearProducto/CrearProducto";
 import VerTenis from "./Components/VerTenis/VerTenis";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,9 +20,9 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home/>}></Route> 
               <Route path="/CrearProducto" element={<CrearProducto/>}></Route>
-              <Route path="/VerTenis" element={<VerTenis
-              
-              />}></Route>
+              <Route path="/VerTenis" element={<VerTenis/>}></Route>
+              <Route path="/Login" element= {<Login/>}></Route>
+      
               <Route path="/ActualizarStock" element={<ActualizarStock/>}></Route>
             </Routes>
         </Router>
